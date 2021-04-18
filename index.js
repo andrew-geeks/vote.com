d9b688c5c8593dc1d7624d980a5affe01d4ba145
Andrew George Issac <andrewgeorgeissac@gmail.com>
2021-04-18 12:10:31 +0530

    v1.0

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 var c1_votes=0;
 var c2_votes=0;
 var c3_votes=0;
+
+$(".btn-1").click(function(){
+    c1_votes++;
+    alert("You have voted for "+$("#cn-1").text());
+})
+
+$(".btn-2").click(function(){
+    c2_votes++;
+    alert("You have voted for "+$("#cn-2").text());
+})
+
+$(".btn-3").click(function(){
+    c3_votes++;
+    alert("You have voted for "+$("#cn-3").text());
+})
+
