3572a3b9e3856b427d0690506bb870c90e6e0e5f
Andrew George Issac <andrewgeorgeissac@gmail.com>
2021-04-22 17:09:15 +0530

    v1.0

--- index.js
+++ index.js
@@ -5,15 +5,25 @@ var c3_votes=0;
 $(".btn-1").click(function(){
     c1_votes++;
     alert("You have voted for "+$("#cn-1").text());
+    sessionStorage.setItem("c1_votes",c1_votes);
 })
 
 $(".btn-2").click(function(){
     c2_votes++;
     alert("You have voted for "+$("#cn-2").text());
+    sessionStorage.setItem("c2_votes",c2_votes);
 })
 
 $(".btn-3").click(function(){
     c3_votes++;
     alert("You have voted for "+$("#cn-3").text());
+    sessionStorage.setItem("c3_votes",c3_votes);
 })
+
+$(".save-btn").click(function(){
+    console.log("success");
+  })
+
+
+  
 
